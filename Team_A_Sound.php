3af4bfd04ecd4f32caba3463928ac2b94d6805df
Luke^^ <lyuboslav.lesichkov@student.nhlstenden.com>
2021-04-14 22:11:06 +0300

    Sound & bar update
    Every team has their own code, which they need to implement , so they get data for this specific robot only, put the code inside your code.
    NOTE: you might be getting errors, because your teams robot, has null value as "data" in the API , so there are testing values included , which could be used to see if the JS code runs
    Read the comments in case of confusion, if you need help write me ^^
    +Changed the first shift of the speed after the start one , to drifting one
    +Sound effects working
    +Speed bar working
    +Getting data from the API is successful now

--- Team_A_Sound.php
+++ Team_A_Sound.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-if (isset($_SESSION['botID']))
-{
-unset($_SESSION['botID']);
+if (isset($_SESSION['botID'])) {
+    unset($_SESSION['botID']);
 }
 ?>
 <!DOCTYPE html>
@@ -11,42 +10,10 @@ unset($_SESSION['botID']);
     <head>
         <title></title>
         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="phptry4.php.php"></script>
-
     </head>
-
-    <style>
-        #myProgress {
-            width: 100%;
-            background-color: #ddd;
-        }
-
-        #myBar {
-            width: 0%;
-            height: 30px;
-            background-color: #4CAF50;
-            text-align: center;
-            line-height: 30px;
-            color: white;
-        }
-        #myProgress1 {
-            width: 100%;
-            background-color: #ddd;
-        }
-
-        #myBar1 {
-            width: 0%;
-            height: 30px;
-            background-color: #4CAF50;
-            text-align: center;
-            line-height: 30px;
-            color: white;
-        }
-    </style>
     <body>
         <!--        this enables the sound :D although it has no value anywhere-->
         <form action="" method="post">
-
             <br>
             <br>
             <input type=submit name="s" value="Enable sound">
@@ -55,15 +22,13 @@ unset($_SESSION['botID']);
         <div id="myProgress">
             <div id="myBar">0%</div>
         </div>
-        <p> Battery </p><div id="myProgress">
-            <div id="myBar1">0%</div>
-        </div>
+ 
         <!--            sound sources-->
         <audio id="start">
             <source src="sound/Acceleration.mp3" type="audio/mpeg">
         </audio>
         <audio id="speed1">
-            <source src="sound/Speed1.mp3" type="audio/mpeg">
+            <source src="sound/SpeedDrifting.mp3" type="audio/mpeg">
         </audio>
         <audio id="speed2">
             <source src="sound/Speed2.mp3" type="audio/mpeg">
@@ -76,81 +41,70 @@ unset($_SESSION['botID']);
         </audio>
 
         <?php
+        //  require_once('navbar.php');
+//making connection with the API 
 
 
 
-
-  //  require_once('navbar.php');
-
-    
-
-        
         $curl = curl_init();
-        
+
         curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://api.samklop.xyz/bots",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "GET",
-          CURLOPT_POSTFIELDS => "",
-      
+            CURLOPT_URL => "https://api.samklop.xyz/bots",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "",
         ));
-        
+
         $response = curl_exec($curl);
         $err = curl_error($curl);
         echo "<pre>";
         echo "</pre>";
         $data = json_decode($response, true);
-        
-        foreach ($data as $bots)
-        {
-            if (is_array($bots))
-            {
-                $sped = $bots[3][4]["data"];
-                $sound = $bots[3][4]["data"]; 
-                if(is_numeric($sped)&& is_numeric($sound))
-                {
-                    $sped = $bots[3][4]["data"];
-                $sound = $bots[3][4]["data"]; 
-                //$_SESSION['Speed2'] = $sped;
-               
-                }
-                else 
-                {
+        //getting data for robot 1 (A)
+        foreach ($data as $bots) { // 
+            if (is_array($bots)) {
+                $sped = $bots[0][1]["data"];
+                $sound = $bots[0][1]["data"];
+                
+                // checking if its int data
+                if (is_numeric($sped) && is_numeric($sound)) {
+                    $sped = $bots[0][1]["data"];
+                    $sound = $bots[0][1]["data"];
+                    $_SESSION['Speed2'] = $sped;
+                } else {
                     $sped = $_SESSION['Speed2'];
                     $sound = $_SESSION['Speed2'];
-                    $sped= 60;
-                    $sound = 40;
                 }
             }
-
         }
         
         
-    
-        
-
+        // test if the functions work with those 2 values
+        // comment them afterwards
+//      $sped = 90;
+//      $sound = 80;
         ?>
-
         <script>
             var i = 0;
-            
-            // the battery starts again if it hits 0
-            if (sessionStorage.getItem("timer") <= 1)
-            {
-                sessionStorage.setItem("timer", "100");
-            }
+
+//            // the battery starts again if it hits 0
+//            if (sessionStorage.getItem("timer") <= 1)
+//            {
+//                sessionStorage.setItem("timer", "100");
+//            }
             if (i == 0) {
                 i = 1;
+                // variables for the sound
                 var Start = document.getElementById("start");
                 var Speed1 = document.getElementById("speed1");
                 var Speed2 = document.getElementById("speed2");
                 var Speed3 = document.getElementById("speed3");
                 var Speed4 = document.getElementById("speed4");
-
+                // functions for sound
                 function playAudioStart() {
                     Start.play();
                 }
@@ -167,44 +121,47 @@ unset($_SESSION['botID']);
                     Speed4.play();
                 }
 
-                var timer2 = localStorage.getItem("seconds");
-                var timer = parseInt(timer2);
+                // THIS FUNCTION IS THE BATTERY FEATURE
+                // THIS FUNCTION DOES NOT WORK PROPERLY
 
-                function Taimer() {
-                    // this is the time removal for the battery
-                    timer = timer - 1;
-                    var x = localStorage.getItem("timer");;
-                    x.toString();
-                    sessionStorage.setItem("timer", x)
-                    document.getElementById("myBar1").innerHTML = timer + "%";
-                    document.getElementById("myBar1").style.width = timer + "%";
-
-                }
-
-                Taimer();
-               
-
+//                var timer2 = localStorage.getItem("seconds");
+//                var timer = parseInt(timer2);
+//
+//                function Taimer() {
+//                    // this is the time removal for the battery
+//                    timer = timer - 1;
+//                    var x = localStorage.getItem("timer");;
+//                    x.toString();
+//                    sessionStorage.setItem("timer", x)
+//                    document.getElementById("myBar1").innerHTML = timer + "%";
+//                    document.getElementById("myBar1").style.width = timer + "%";
+//
+//                }
+//
+//                Taimer();
+//                var time = setInterval(Taimer, 5000);
 
                 //this changes the speed bar up/down
                 var elem = document.getElementById("myBar");
                 var id = setInterval(myFunction, 20);
                 var sound = "<?php echo($sound); ?>";
                 sound.toString();
-                sessionStorage.setItem("sound", sound)
+                sessionStorage.setItem("sound", sound);
                 function myFunction() {
                     if (sound >= sound2) {
                         // it goes up
-                        sound++;
-                        elem.style.width = sound + "%";
-                        elem.innerHTML = sound + "%";
-                        var sound2 = sound;
-                        sound2.toString();
-                        sessionStorage.setItem("sound2", sound2)
+                        sound2++;
+                        elem.style.width = sound2 + "%";
+                        elem.innerHTML = sound2 + "%";
+
                     } else {
                         // goes down
                         document.getElementById("myBar").innerHTML = sound + "%";
                         document.getElementById("myBar").style.width = sound + "%";
                     }
+                    var sound2 = sound;
+                    sound2.toString();
+                    sessionStorage.setItem("sound2", sound2)
 
                 }
 
@@ -213,44 +170,33 @@ unset($_SESSION['botID']);
 
 
 
-            // reloads the page every 5 seconds
-            var timeout = setTimeout("location.reload(true);", 5000);
+            // reloads the page every 10 seconds
+            var timeout = setTimeout("location.reload(true);", 10000);
+
             //updates the div for music
             function AudioReload() {
                 $("#here").load(window.location.href + " #here");
             }
-
             var Audio = setInterval(AudioReload, 3000);
 
 
         </script>
         <div id="here">  
 <?php
-// the div for music, with a random variable
-
+// the music conditions
 if ($sped <= 10) {
     echo "<script> playAudioStart(); </script>";
-    $sped = 150;
 } else if ($sped > 10 && $sped < 20) {
     echo "<script> playAudioSpeed1(); </script>";
-    $sped = 150;
 } else if ($sped >= 20 && $sped <= 40) {
     echo "<script> playAudioSpeed2(); </script>";
-    $sped = 150;
 } else if ($sped > 40 && $sped <= 70) {
     echo "<script> playAudioSpeed3(); </script>";
-    $sped = 150;
 } else if ($sped > 70 && $sped <= 100) {
     echo "<script> playAudioSpeed4(); </script>";
-    $sped = 150;
 }
 ?>
 
         </div>
-
-
-
-
-
     </body>
 </html>
---
+++ Team_E_Sound.php
@@ -0,0 +1,219 @@
+<?php
+session_start();
+if (isset($_SESSION['botID']))
+{
+unset($_SESSION['botID']);
+}
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title></title>
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+    </head>
+
+    <body>
+        <!--        this enables the sound :D although it has no value anywhere-->
+        <form action="" method="post">
+
+            <br>
+            <br>
+            <input type=submit name="s" value="Enable sound">
+        </form>
+
+        <div id="myProgress">
+            <div id="myBar">0%</div>
+        </div>
+        <!--            sound sources-->
+        <audio id="start">
+            <source src="sound/Acceleration.mp3" type="audio/mpeg">
+        </audio>
+        <audio id="speed1">
+            <source src="sound/SpeedDrifting.mp3" type="audio/mpeg">
+        </audio>
+        <audio id="speed2">
+            <source src="sound/Speed2.mp3" type="audio/mpeg">
+        </audio>
+        <audio id="speed3">
+            <source src="sound/Speed3.mp3" type="audio/mpeg">
+        </audio>
+        <audio id="speed4">
+            <source src="sound/Speed4.mp3" type="audio/mpeg">
+        </audio>
+
+        <?php
+
+  //  require_once('navbar.php');
+//making connection with the API 
+           
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://api.samklop.xyz/bots",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_POSTFIELDS => "",
+      
+        ));
+        
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        echo "<pre>";
+        echo "</pre>";
+        $data = json_decode($response, true);
+        //getting data for robot 5 (E)
+        foreach ($data as $bots)
+        {
+            if (is_array($bots))
+            {
+                $sped = $bots[4][5]["data"];
+                $sound = $bots[4][5]["data"]; 
+                // checking if its int data
+                if(is_numeric($sped)&& is_numeric($sound))
+                {
+                $sped = $bots[4][5]["data"];
+                $sound = $bots[4][5]["data"]; 
+                $_SESSION['Speed2'] = $sped;
+                }
+                else 
+                {
+                    $sped = $_SESSION['Speed2'];
+                    $sound = $_SESSION['Speed2'];
+
+                }
+            }
+
+        }
+        // test if the functions work with those 2 values
+        // comment them afterwards
+//      $sped = 90;
+//      $sound = 80;
+
+
+        ?>
+
+        <script>
+            var i = 0;
+            
+//            // the battery starts again if it hits 0
+//            if (sessionStorage.getItem("timer") <= 1)
+//            {
+//                sessionStorage.setItem("timer", "100");
+//            }
+            if (i == 0) {
+                i = 1;
+                // variables for the sound
+                var Start = document.getElementById("start");
+                var Speed1 = document.getElementById("speed1");
+                var Speed2 = document.getElementById("speed2");
+                var Speed3 = document.getElementById("speed3");
+                var Speed4 = document.getElementById("speed4");
+                // functions for sound
+                function playAudioStart() {
+                    Start.play();
+                }
+                function playAudioSpeed1() {
+                    Speed1.play();
+                }
+                function playAudioSpeed2() {
+                    Speed2.play();
+                }
+                function playAudioSpeed3() {
+                    Speed3.play();
+                }
+                function playAudioSpeed4() {
+                    Speed4.play();
+                }
+
+                // THIS FUNCTION IS THE BATTERY FEATURE
+                // THIS FUNCTION DOES NOT WORK PROPERLY
+                
+//                var timer2 = localStorage.getItem("seconds");
+//                var timer = parseInt(timer2);
+//
+//                function Taimer() {
+//                    // this is the time removal for the battery
+//                    timer = timer - 1;
+//                    var x = localStorage.getItem("timer");;
+//                    x.toString();
+//                    sessionStorage.setItem("timer", x)
+//                    document.getElementById("myBar1").innerHTML = timer + "%";
+//                    document.getElementById("myBar1").style.width = timer + "%";
+//
+//                }
+//
+//                Taimer();
+//                var time = setInterval(Taimer, 5000);
+
+                //this changes the speed bar up/down
+                var elem = document.getElementById("myBar");
+                var id = setInterval(myFunction, 20);
+                var sound = "<?php echo($sound); ?>";
+                sound.toString();
+                sessionStorage.setItem("sound", sound);
+                function myFunction() {
+                    if (sound >= sound2) {
+                        // it goes up
+                        sound2++;
+                        elem.style.width = sound2 + "%";
+                        elem.innerHTML = sound2 + "%";
+                     
+                    } else {
+                        // goes down
+                        document.getElementById("myBar").innerHTML = sound + "%";
+                        document.getElementById("myBar").style.width = sound + "%";
+                    }
+                       var sound2 = sound;
+                        sound2.toString();
+                        sessionStorage.setItem("sound2", sound2)
+
+                }
+
+            }
+
+
+            // reloads the page every 10 seconds
+            var timeout = setTimeout("location.reload(true);", 10000);
+            
+            //updates the div for music
+            function AudioReload() {
+                $("#here").load(window.location.href + " #here");
+            }
+
+            var Audio = setInterval(AudioReload, 3000);
+
+
+        </script>
+        <div id="here">  
+<?php
+// the music conditions
+
+    
+if ($sped <= 10) {
+    echo "<script> playAudioStart(); </script>";
+    
+} else if ($sped > 10 && $sped < 20) {
+    echo "<script> playAudioSpeed1(); </script>";
+   
+} else if ($sped >= 20 && $sped <= 40) {
+    echo "<script> playAudioSpeed2(); </script>";
+   
+} else if ($sped > 40 && $sped <= 70) {
+    echo "<script> playAudioSpeed3(); </script>";
+    
+} else if ($sped > 70 && $sped <= 100) {
+    echo "<script> playAudioSpeed4(); </script>";
+    
+}
+
+?>
+        </div>
+        
+    </body>
+</html>
