6059ff3ad056dfb13786f22c0951601f3997cace
Edward <24864616+samklop@users.noreply.github.com>
2021-04-14 23:33:32 +0200

    Group1 page fixes

--- group1.php
+++ group1.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_SESSION['botID'] = "1";
+$botID = "1";
 ?>
 <!doctype html>
 <html lang="en">
@@ -13,165 +13,67 @@ $_SESSION['botID'] = "1";
 
 <body class="min-h-full bg-gray-800 text-white">
     <?php
-    if (isset($_GET["backward"]) && isset($_SESSION['botID']))
+    function setTask($task, $botid)
     {
-        $command = $_GET["backward"];
-        //The url you wish to send the POST request to (change url)
         $url = "https://api.samklop.xyz/settask";
-        //The data you want to send via POST look at manual of api
-        $data = array('id' => $_SESSION['botID'], 'task' => $command);
-        //url-ify the data for the POST
-        $data_string = http_build_query($data);
-        //open connection
+        $data = ['id' => $botid, 'task' => $task];
+        $dataString = http_build_query($data);
+
         $ch = curl_init();
-        //set the url, number of POST vars, POST data
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //execute post
-        $result = curl_exec($ch);
-        $info = curl_getinfo($ch);
+
+        curl_exec($ch);
     }
 
-    if (isset($_GET["stopMotor"]) && isset($_SESSION['botID']))
-    {
-        $command = $_GET["stopMotor"];
-        //The url you wish to send the POST request to (change url)
-        $url = "https://api.samklop.xyz/settask";
-        //The data you want to send via POST look at manual of api
-        $data = array('id' => $_SESSION['botID'], 'task' => $command);
-        //url-ify the data for the POST
-        $data_string = http_build_query($data);
-        //open connection
-        $ch = curl_init();
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //execute post
-        $result = curl_exec($ch);
-        $info = curl_getinfo($ch);
-    }
+    if (isset($_GET['command']) && !empty($_GET['command']))
+        $command = $_GET["command"];
+    else
+        $command = null;
 
-    if (isset($_GET["right"]) && isset($_SESSION['botID']))
-    {
-        $command = $_GET["right"];
-        //The url you wish to send the POST request to (change url)
-        $url = "https://api.samklop.xyz/settask";
-        //The data you want to send via POST look at manual of api
-        $data = array('id' => $_SESSION['botID'], 'task' => $command);
-        //url-ify the data for the POST
-        $data_string = http_build_query($data);
-        //open connection
-        $ch = curl_init();
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //execute post
-        $result = curl_exec($ch);
-        $info = curl_getinfo($ch);
-    }
-
-    if (isset($_GET["left"]) && isset($_SESSION['botID']))
-    {
-        $command = $_GET["left"];
-        //The url you wish to send the POST request to (change url)
-        $url = "https://api.samklop.xyz/settask";
-        //The data you want to send via POST look at manual of api
-        $data = array('id' => $_SESSION['botID'], 'task' => $command);
-        //url-ify the data for the POST
-        $data_string = http_build_query($data);
-        //open connection
-        $ch = curl_init();
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //execute post
-        $result = curl_exec($ch);
-        $info = curl_getinfo($ch);
-    }
-
-    if (isset($_GET["forward"]) && isset($_SESSION['botID']))
-    {
-        $command = $_GET["forward"];
-        //The url you wish to send the POST request to (change url)
-        $url = "https://api.samklop.xyz/settask";
-        //The data you want to send via POST look at manual of api
-        $data = array('id' => $_SESSION['botID'], 'task' => $command);
-        //url-ify the data for the POST
-        $data_string = http_build_query($data);
-        //open connection
-        $ch = curl_init();
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //execute post
-        $result = curl_exec($ch);
-        $info = curl_getinfo($ch);
-    }
+    if (isset($_GET["backward"]))
+        setTask('backward', $botID);
+    else if (isset($_GET["stopMotor"]))
+        setTask('stopmotor', $botID);
+    else if (isset($_GET["right"]))
+        setTask('right', $botID);
+    else if (isset($_GET["left"]))
+        setTask('left', $botID);
+    else if (isset($_GET["forward"]))
+        setTask('forward', $botID);
+    else
+        setTask($command, $botID);
 
     require_once('navbar.php');
-    if (isset($_GET["command"]) && isset($_SESSION['botID']))
-    {
-        $command = $_GET["command"];
-        //The url you wish to send the POST request to (change url)
-        $url = "https://api.samklop.xyz/settask";
-        //The data you want to send via POST look at manual of api
-        $data = array('id' => $_SESSION['botID'], 'task' => $command);
-        //url-ify the data for the POST
-        $data_string = http_build_query($data);
-        //open connection
-        $ch = curl_init();
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //execute post
-        $result = curl_exec($ch);
-        $info = curl_getinfo($ch);
-    }
     ?>
     <div class="flex justify-center m-5">
         <div class="flex justify-center flex-col">
             <div class="bg-gray-600 p-2 rounded">
-                <img src="inc/arduino.jpg" alt="Livestream">
+                <img src="http://foscam.serverict.nl/snapshot.cgi?&user=gast&pwd=gast&t=" class="livestreamVideo" name="refresh" id="refresh" onload="reload(this)" onerror="reload(this)">
             </div>
-            <form class="bg-gray-600 p-2 rounded m-2 text-center">
-                <select name="command" class="bg-gray-800 p-2 rounded">
+            <form class="bg-gray-600 p-2 rounded m-2 text-center" action=<?= $_SERVER['PHP_SELF'] ?> method="get">
+                <select name="command" class="bg-gray-800 p-2 rounded text-white">
                     <?php
-                    $the_key = "race"; // or whatever you want
-                    if (isset($_GET["command"]))
-                        $the_key = $command;
+                    if ($command === null)
+                        $command = 'race';
 
                     foreach (["Race", "TikTacToe", "Maze", "ShapeRedraw", "Idle"] as $val) : ?>
-                        <option value="<?= strtolower($val) ?>" <?= (strtolower($val) === $the_key) ? 'selected="selected"' : ''; ?>>
+                        <option value="<?= strtolower($val) ?>" <?= (strtolower($val) === $command) ? 'selected="selected"' : ''; ?>>
                             <?= $val ?>
                         </option>
                     <?php endforeach; ?>
                 </select>
-                <input type="submit" class="bg-gray-800 p-2 rounded" value="Send command">
-
+                <input type="submit" class="bg-gray-800 p-2 rounded text-white" value="Send command">
+            </form>
+            <form class="bg-gray-600 p-2 rounded m-2 text-center" action=<?= $_SERVER['PHP_SELF'] ?> method="get">
                 <div class="flex justify-center flex-row">
-                    <input type="submit" name="forward" class="bg-gray-600 p-2 rounded" value="Forward">
-                    <input type="submit" name="left" class="bg-gray-600 p-2 rounded" value="Left">
-                    <input type="submit" name="right" class="bg-gray-600 p-2 rounded" value="Right">
-                    <input type="submit" name="backward" class="bg-gray-600 p-2 rounded" value="Backward">
-                    <input type="submit" name="stopMotor" class="bg-gray-600 p-2 rounded" value="Stop Motor">
+                    <input type="submit" name="forward" class="bg-gray-600 p-2 rounded text-white" value="Forward">
+                    <input type="submit" name="left" class="bg-gray-600 p-2 rounded text-white" value="Left">
+                    <input type="submit" name="right" class="bg-gray-600 p-2 rounded text-white" value="Right">
+                    <input type="submit" name="backward" class="bg-gray-600 p-2 rounded text-white" value="Backward">
+                    <input type="submit" name="stopMotor" class="bg-gray-600 p-2 rounded text-white" value="Stop Motor">
                 </div>
             </form>
         </div>
@@ -185,11 +87,11 @@ $_SESSION['botID'] = "1";
     </div>
     <script>
         var audio = new Audio('sound/speaker-test-sample.wav');
+        audio.loop = true;
 
         function sound() {
             if (audio.paused) {
                 audio.play();
-                audio.loop = true;
             } else {
                 audio.pause();
             }
